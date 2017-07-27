b7733ed08ff8abcda7fdc3ed2f452aa940663f8e
Tom Courtney <tpcourtney@gmail.com>
2017-07-27 16:57:56 -0400

    k means cross validation added and working

--- ann.py
+++ ann.py
@@ -91,6 +91,27 @@ new_prediction = classifier.predict(sc.transform(np.array([[0,0, 600, 1, 40, 3, 
 print(new_prediction, (new_prediction>0.5))
 
 
+# Evaluating improving and tuning the ADD
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(output_dim=6, init = 'uniform', activation = 'relu', input_dim=11 ))
+    classifier.add(Dense(output_dim=6, init = 'uniform', activation = 'relu'))
+    classifier.add(Dense(output_dim=1, init = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adam', loss='binary_crossentropy', metrics= ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn=build_classifier, batch_size=10, nb_epoch=10)
+accuracies = cross_val_score(estimator = classifier, X=X_train, y=y_train, cv=10)
+mean = accuracies.mean()
+variance =accuracies.std()
+
+
+# Dropout regularization to reduce overfitting if needed
 
 
 
